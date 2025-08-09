73873113bcbada649047bd7f75717720f9f224de
Shubham Bajaj <shubham2bajaj@gmail.com>
2025-08-09 18:07:32 +0200

    remove max:100 attribute on y axis to display wide range of values

--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -21,50 +21,12 @@ const LineChart = ({ dataset }: { dataset: DataPoint[] }) => {
     yAxis: {
       title: { text: "Value" },
       min: 0,
-      max: 100,
     },
 
     series: [
       {
-        // data: dataset.map((point) => [point.x, typeof point.y !== "number" ? 0 : point.y])
-        data: [
-          {
-            x: 0,
-            y: 59.5
-          },
-          {
-            x: 5,
-            y: 19.8
-          },
-          {
-            x: 10,
-            y: "65.16"
-          },
-          {
-            x: 15,
-            y: 14.34
-          },
-          {
-            x: 20,
-            y: undefined
-          },
-          {
-            x: 25,
-            y: 41.05
-          },
-          {
-            x: 30,
-            y: 67.68
-          },
-          {
-            x: 35,
-            y: 91.27
-          },
-          {
-            x: 40,
-            y: 94.42
-          }
-        ].map((point) => [point.x, typeof point.y !== "number" ? 0 : point.y])
+        data: dataset.map((point) => [point.x, typeof point.y !== "number" ? 0 : point.y])
+        // data: [{ x: 0, y: 59.5 }, { x: 5, y: 19.8 }, { x: 10, y: "65.16" }, { x: 15, y: 14.34 }, { x: 20, y: undefined }, { x: 25, y: 41.05 }, { x: 30, y: 67.68 }, { x: 35, y: 91.27 }, { x: 40, y: 94.42 }].map((point) => [point.x, typeof point.y !== "number" ? 0 : point.y])
       }
     ]
   };
